a67a9d29474fe0f8cae05d019748d17730bfaa23
Danniel Libor <dlibor.dev@gmail.com>
2025-03-31 15:13:28 +0800

    cart

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -5,12 +5,18 @@
         <tr>
           <th class="text-left">Product Name</th>
           <th class="text-left">Quantity</th>
+          <th class="text-left">Action</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="item in items" :key="item.id">
           <td>{{ item.product.name }}</td>
           <td>{{ item.quantity }}</td>
+          <td>
+            <v-btn color="red" variant="text" @click="removeItem(item.id)">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </td>
         </tr>
       </tbody>
     </v-table>
